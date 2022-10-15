5269d65b65a6c9f22384c222cdd96940b0a2b0e3
Domingos Nunes <domingos.nunes@atlar.pt>
2022-10-15 16:40:25 +0100

    ST-1400: elasticsearch cluster works with mTLS/SSL

--- Makefile
+++ Makefile
@@ -23,6 +23,8 @@ vars:  ## Variables
 	@echo "PLAYBOOKS_ROOT_DIR=$(PLAYBOOKS_ROOT_DIR)"
 	@echo "ANSIBLE_CONFIG=$(ANSIBLE_CONFIG)"
 	@echo "PLAYBOOKS_HOSTS=$(PLAYBOOKS_HOSTS)"
+	@echo "CA_CERT_PASS=$(CA_CERT_PASS)"
+	@echo "ELASTIC_PASSWORD=$(ELASTIC_PASSWORD)"
 
 ping:  check-env ## Ping Ansible targets
 	ansible all -i $(PLAYBOOKS_ROOT_DIR)/inventory.yml -m ping -l $(PLAYBOOKS_HOSTS)
--- resources/jobs/elastic.mk
+++ resources/jobs/elastic.mk
@@ -13,10 +13,15 @@ ifndef CA_CERT_PASS
 	$(error CA_CERT_PASS is undefined)
 endif
 
-install: check_hosts check_ca_pass ## Install elastic
+check_elastic_pass:
+ifndef ELASTIC_PASSWORD
+	$(error ELASTIC_PASSWORD is undefined)
+endif
+
+install: check_hosts check_ca_pass check_elastic_pass ## Install elastic
 	ansible-playbook ./ansible_collections/ska_collections/elastic/playbooks/install.yml \
 	-i $(PLAYBOOKS_ROOT_DIR)/inventory.yml \
-	--extra-vars "target_hosts=$(PLAYBOOKS_HOSTS) ca_cert_pass=$(CA_CERT_PASS)"
+	--extra-vars "target_hosts=$(PLAYBOOKS_HOSTS) ca_cert_pass=$(CA_CERT_PASS) elastic_password=$(ELASTIC_PASSWORD)"
 	
 destroy: check_hosts ## Destroy elastic cluster
 	ansible-playbook ./ansible_collections/ska_collections/elastic/playbooks/destroy.yml \
