4163b31a082aed0390e03de2ad735a49152133de
J. Bruno Morgado <jb.morgado@gmail.com>
2022-10-26 10:32:36 +0100

    ST-1350: Call elastic plybooks, fix documentation

--- resources/jobs/elastic.mk
+++ resources/jobs/elastic.mk
@@ -58,11 +58,11 @@ help: ## Show Help
 	@grep -E '^[0-9a-zA-Z_-]+:.*?## .*$$' $(MAKEFILE_LIST) | sort | awk 'BEGIN {FS = ": .*?## "}; {printf "\033[36m%-30s\033[0m %s\n", $$1, $$2}'
 
 ## API targets
-elastic-check: ## Check Elastic API status
+check: ## Check Elastic API status
 	@ssh -qt -i $(PEM_FILE) ubuntu@$(LOADBALANCER_IP) \
 		"sudo curl -X GET -u $(ELASTIC_USER):$(ELASTIC_PASSWORD) --cacert $(CA_CERT) --cert $(CERT) --key $(CERT_KEY) '$(LOGGING_URL)/_cat/health?pretty'"
 
-elastic-key-list: ## List existing Elastic API keys
+key-list: ## List existing Elastic API keys
 	@echo "Existing API keys:"
 	@echo ""
 	@ssh -qt -i $(PEM_FILE) ubuntu@$(LOADBALANCER_IP) \
@@ -70,56 +70,56 @@ elastic-key-list: ## List existing Elastic API keys
 
 ifdef KEY_NAME
 ifdef KEY_EXPIRATION
-elastic-key-new: ## Create new key. Pass name of the key as KEY_NAME=xxx. Pass optional expiration as KEY_EXPIRATION=xxx.
+key-new: ## Create new key. Pass name of the key as KEY_NAME=xxx. Pass optional expiration as KEY_EXPIRATION=xxx.
 	@echo "Create API key with expiration date:"
 	@echo ""
 	@ssh -qt -i $(PEM_FILE) ubuntu@$(LOADBALANCER_IP) \
 		"sudo curl -X POST -u $(ELASTIC_USER):$(ELASTIC_PASSWORD) --cacert $(CA_CERT) --cert $(CERT) --key $(CERT_KEY) '$(LOGGING_URL)/_security/api_key?pretty' -H 'Content-Type: application/json' -d'{\"name\": \"$(KEY_NAME)\", \"expiration\": \"$(KEY_EXPIRATION)\"}'"
 else
-elastic-key-new:
+key-new:
 	@echo "Create API key without expiration date:"
 	@echo ""
 	@ssh -qt -i $(PEM_FILE) ubuntu@$(LOADBALANCER_IP) \
 		"sudo curl -X POST -u $(ELASTIC_USER):$(ELASTIC_PASSWORD) --cacert $(CA_CERT) --cert $(CERT) --key $(CERT_KEY) '$(LOGGING_URL)/_security/api_key?pretty' -H 'Content-Type: application/json' -d'{\"name\": \"$(KEY_NAME)\"}'"
 endif
 else
-elastic-key-new:
+key-new:
 	@echo "ERROR: Please pass name of the key as KEY_NAME=xxx"
 	@exit 1
 endif
 
 ifdef KEY_ID
-elastic-key-info: ## Get API key info. Pass the id of the key as KEY_ID=xxx
+key-info: ## Get API key info. Pass the id of the key as KEY_ID=xxx
 	@echo "Information on API key: $(KEY_ID)"
 	@echo ""
 	@ssh -qt -i $(PEM_FILE) ubuntu@$(LOADBALANCER_IP) \
 		"sudo curl -X GET -u $(ELASTIC_USER):$(ELASTIC_PASSWORD) --cacert $(CA_CERT) --cert $(CERT) --key $(CERT_KEY) '$(LOGGING_URL)/_security/api_key?id=$(KEY_ID)&pretty'"
 else
-elastic-key-info:
+key-info:
 	@echo "ERROR: Please pass id of the key as KEY_ID=xxx"
 	@exit 1
 endif
 
 ifdef KEY_ID
-elastic-key-invalidate: ## Invalidate API key. Pass the id of the key as KEY_ID=xxx
+key-invalidate: ## Invalidate API key. Pass the id of the key as KEY_ID=xxx
 	@echo "Delete API key: $(KEY_ID)"
 	@echo ""
 	@ssh -qt -i $(PEM_FILE) ubuntu@$(LOADBALANCER_IP) \
 		"sudo curl -X DELETE -u $(ELASTIC_USER):$(ELASTIC_PASSWORD) --cacert $(CA_CERT) --cert $(CERT) --key $(CERT_KEY) '$(LOGGING_URL)/_security/api_key?pretty' -H 'Content-Type: application/json' -d'{\"ids\": \"$(KEY_ID)\"}'"
 else
-elastic-key-invalidate:
+key-invalidate:
 	@echo "ERROR: Please pass id of the key as KEY_ID=xxx"
 	@exit 1
 endif
 
 ifdef KEY
-elastic-key-query: ## Query health using API key. Pass the encoded API Key as KEY=xxx
-	@echo "Query cluster health using encoded API key:"
+key-query: ## Query health using API key. Pass the encoded API Key as KEY=xxx
+	@echo "Query cluster health using encoded API key: $(KEY)"
 	@echo ""
 	@ssh -qt -i $(PEM_FILE) ubuntu@$(LOADBALANCER_IP) \
 		"sudo curl -X GET --cacert $(CA_CERT) --cert $(CERT) --key $(CERT_KEY) '$(LOGGING_URL)/_cat/health?pretty' -H 'authorization: ApiKey $(KEY)'"
 else
-elastic-key-query:
+key-query:
 	@echo "ERROR: Please pass encoded API key as KEY=xxx"
 	@exit 1
 endif
