d8a5c5f201ef82b0ef712569239213a3837d3ca4
Matteo Di Carlo <matteo.dicarlo@inaf.it>
2022-11-03 12:06:09 +0000

    ST-1366 fixes
    Signed-off-by: Matteo Di Carlo <matteo.dicarlo@inaf.it>

--- resources/jobs/monitoring.mk
+++ resources/jobs/monitoring.mk
@@ -80,6 +80,7 @@ prometheus: check_hosts ## Install Prometheus Server
 		-e "prometheus_gitlab_ci_pipelines_exporter_token=$(GITLAB_TOKEN)" \
 		-e "ca_cert_password=$(CA_CERT_PASSWORD)" \
 		-e @$(PROM_CONFIGS_PATH)/prometheus_node_metric_relabel_configs.yaml \
+		-e @$(PROM_CONFIGS_PATH)/ansible_collections/ska_collections/monitoring/group_vars/all.yml \
 		-e @$(PLAYBOOKS_ROOT_DIR)/group_vars/all.yml \
 		-e @$(PLAYBOOKS_ROOT_DIR)/group_vars/prometheus.yml \
 		-e "target_hosts='$(PLAYBOOKS_HOSTS)'" \
@@ -91,6 +92,7 @@ grafana: check_hosts ## Install Grafana Server
 		$(ANSIBLE_PLAYBOOK_ARGUMENTS) \
 		-e "mode='grafana'" \
 		--extra-vars="azuread_client_id='$(AZUREAD_CLIENT_ID)' azuread_client_secret='$(AZUREAD_CLIENT_SECRET)' azuread_tenant_id='$(AZUREAD_TENANT_ID)'" \
+		-e @$(PROM_CONFIGS_PATH)/ansible_collections/ska_collections/monitoring/group_vars/all.yml \
 		-e @$(PROM_CONFIGS_PATH)/prometheus_node_metric_relabel_configs.yaml \
 		-e @$(PLAYBOOKS_ROOT_DIR)/group_vars/all.yml \
 		-e @$(PLAYBOOKS_ROOT_DIR)/group_vars/prometheus.yml \
@@ -102,6 +104,7 @@ alertmanager: check_hosts ## Install Prometheus Server
 		-i $(INVENTORY) \
 		$(ANSIBLE_PLAYBOOK_ARGUMENTS) \
 		-e "mode='alert' slack_api_url='$(SLACK_API_URL)' slack_api_url_user='$(SLACK_API_URL_USER)'" \
+		-e @$(PROM_CONFIGS_PATH)/ansible_collections/ska_collections/monitoring/group_vars/all.yml \
 		-e @$(PLAYBOOKS_ROOT_DIR)/group_vars/all.yml \
 		-e @$(PLAYBOOKS_ROOT_DIR)/group_vars/prometheus.yml \
 		-e @$(PROM_CONFIGS_PATH)/prometheus_node_metric_relabel_configs.yaml \
@@ -116,6 +119,7 @@ thanos: check_hosts ## Install Thanos query and query front-end
 		-e "ca_cert_password=$(CA_CERT_PASSWORD)" \
 		-e "project_id='$(PROM_OS_PROJECT_ID)' auth_url='$(PROM_OS_AUTH_URL)'" \
 		-e "username='$(PROM_OS_USERNAME)' password='$(PROM_OS_PASSWORD)'" \
+		-e @$(PROM_CONFIGS_PATH)/ansible_collections/ska_collections/monitoring/group_vars/all.yml \
 		-e @$(PLAYBOOKS_ROOT_DIR)/group_vars/all.yml \
 		-e @$(PLAYBOOKS_ROOT_DIR)/group_vars/prometheus.yml \
 		-e "target_hosts='$(PLAYBOOKS_HOSTS)'" \
@@ -140,7 +144,7 @@ update_metadata: check_hosts ## OpenStack metadata for node_exporters - pass INV
 	rm -rf combined_inventory all_inventory
 
 update_scrapers: check_hosts ## Force update of scrapers
-	@ansible -i $(INVENTORY) $(PROMETHEUS_NODE) $(ANSIBLE_PLAYBOOK_ARGUMENTS) -b -m shell -a 'export project_id=$(PROM_OS_PROJECT_ID) project_name=$(PROM_OS_PROJECT_NAME) auth_url=$(PROM_OS_AUTH_URL) username=$(PROM_OS_USERNAME) password=$(PROM_OS_PASSWORD) $(OPENSTACK_ENV_VARIABLES) && cd /etc/prometheus && python3 /usr/local/bin/prom_helper.py -g'
+	ansible -i $(INVENTORY) $(PROMETHEUS_NODE) $(ANSIBLE_PLAYBOOK_ARGUMENTS) -b -m shell -a 'export project_id=$(PROM_OS_PROJECT_ID) project_name=$(PROM_OS_PROJECT_NAME) auth_url=$(PROM_OS_AUTH_URL) username=$(PROM_OS_USERNAME) password=$(PROM_OS_PASSWORD) $(OPENSTACK_ENV_VARIABLES) && cd /etc/prometheus && python3 /usr/local/bin/prom_helper.py -g'
 
 help: ## Show Help
 	@echo "Monitoring targets - make playbooks monitoring <target>:"
