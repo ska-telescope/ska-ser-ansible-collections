070825a7630bc0fc7406b2ba3f93401bebb8a4b4
Piers Harding <piers@ompka.net>
2022-12-30 18:07:29 +0000

    BANG-105: add BYOH support for clusterapi
    Signed-off-by: Piers Harding <piers@ompka.net>

--- resources/jobs/clusterapi.mk
+++ resources/jobs/clusterapi.mk
@@ -1,49 +1,73 @@
+
+clusterapi-check-hosts:
 ifndef PLAYBOOKS_HOSTS
 	$(error PLAYBOOKS_HOSTS is undefined)
 endif
 
+clusterapi-check-cluster-type:
+ifndef CLUSTERAPI_CLUSTER_TYPE
+	$(error CLUSTERAPI_CLUSTER_TYPE is undefined - MUST be 'capo' or 'byoh')
+endif
+
+CLUSTERAPI_APPLY ?= false ## Apply workload cluster: true or false - default: false
+
+
 .DEFAULT_GOAL := clusterapi
+
+vars:
+	@echo "\033[36mclusterapi:\033[0m"
+	@echo "INVENTORY=$(INVENTORY)"
+	@echo "PLAYBOOKS_HOSTS=$(PLAYBOOKS_HOSTS)"
 
 clusterapi-install-base:  ## Install base for management server
 	ansible-playbook ./ansible_collections/ska_collections/docker_base/playbooks/containers.yml \
-	-i $(PLAYBOOKS_ROOT_DIR) \
+	-i $(INVENTORY) \
 	--extra-vars "target_hosts=$(PLAYBOOKS_HOSTS)" \
 	--limit "management-cluster"
 
+# clusterapi-management:
 clusterapi-management: clusterapi-install-base  ## Install Minikube management cluster
 	ansible-playbook ./ansible_collections/ska_collections/minikube/playbooks/minikube.yml \
-	-i $(PLAYBOOKS_ROOT_DIR) \
+	-i $(INVENTORY) \
 	--extra-vars "target_hosts=$(PLAYBOOKS_HOSTS)" \
 	--limit "management-cluster" --tags "build"
 
-# clusterapi:
-clusterapi: clusterapi-management clusterapi-velero-backups  ## Install clusterapi component
+clusterapi:
+# clusterapi: clusterapi-check-hosts clusterapi-management clusterapi-velero-backups  ## Install clusterapi component
 	ansible-playbook ./ansible_collections/ska_collections/clusterapi/playbooks/clusterapi.yml \
-	-i $(PLAYBOOKS_ROOT_DIR) \
+	-i $(INVENTORY) \
 	--extra-vars "target_hosts=$(PLAYBOOKS_HOSTS)" \
 	--limit "management-cluster"
 
-clusterapi-createworkload:  ## Template workload manifest and deploy
+clusterapi-createworkload: clusterapi-check-cluster-type  ## Template workload manifest and deploy
 	ansible-playbook ./ansible_collections/ska_collections/clusterapi/playbooks/createworkload.yml \
-	-i $(PLAYBOOKS_ROOT_DIR) \
+	-i $(INVENTORY) \
 	--extra-vars "target_hosts=$(PLAYBOOKS_HOSTS)" \
+	--extra-vars "capi_kustomize_overlay=$(CLUSTERAPI_CLUSTER_TYPE)" \
+	--extra-vars '{"cluster_apply": $(CLUSTERAPI_APPLY)}' \
+	--extra-vars '{"capi_cluster_extra_vars": {$(CLUSTERAPI_CLUSTER_EXTRA_VARS)} }' \
 	--limit "management-cluster"
+
+clusterapi-byoh:  ## Template workload manifest and deploy
+	ansible-playbook ./ansible_collections/ska_collections/clusterapi/playbooks/byoh.yml \
+	-i $(INVENTORY) \
+	--extra-vars "target_hosts=$(PLAYBOOKS_HOSTS)" -v
 
 clusterapi-destroy-management:  ## Destroy Minikube management cluster
 	ansible-playbook ./ansible_collections/ska_collections/minikube/playbooks/minikube.yml \
-	-i $(PLAYBOOKS_ROOT_DIR) \
+	-i $(INVENTORY) \
 	--extra-vars "target_hosts=$(PLAYBOOKS_HOSTS)" \
 	--limit "management-cluster" --tags "destroy"
 
 clusterapi-imagebuilder:  ## Build and upload OS Image Builder Kubernetes image
 	ansible-playbook ./ansible_collections/ska_collections/clusterapi/playbooks/imagebuilder.yml \
-	-i $(PLAYBOOKS_ROOT_DIR) \
+	-i $(INVENTORY) \
 	--extra-vars "target_hosts=$(PLAYBOOKS_HOSTS)" \
 	--limit "management-cluster" -v
 
 clusterapi-velero-backups:  ## Configure Velero backups on Minikube management cluster
 	ansible-playbook ./ansible_collections/ska_collections/minikube/playbooks/velero_backups.yml \
-	-i $(PLAYBOOKS_ROOT_DIR) \
+	-i $(INVENTORY) \
 	--extra-vars "target_hosts=$(PLAYBOOKS_HOSTS)" \
 	--extra-vars "cloud_config=$(CLUSTERAPI_CLOUD_CONFIG)" \
 	--extra-vars "cloud=$(CLUSTERAPI_CLOUD)" \
