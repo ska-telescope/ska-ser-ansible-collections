329ef6145cc8fe2e4fa7a1c3e21f75374360032c
Piers Harding <piers@ompka.net>
2023-01-04 12:19:08 +0000

    BANG-105: pass thru ansible vars
    Signed-off-by: Piers Harding <piers@ompka.net>

--- resources/jobs/clusterapi.mk
+++ resources/jobs/clusterapi.mk
@@ -8,6 +8,9 @@ clusterapi-check-cluster-type:
 ifndef CLUSTERAPI_CLUSTER_TYPE
 	$(error CLUSTERAPI_CLUSTER_TYPE is undefined - MUST be 'capo' or 'byoh')
 endif
+ANSIBLE_PLAYBOOK_ARGUMENTS ?=
+ANSIBLE_EXTRA_VARS ?=
+PLAYBOOKS_DIR ?= ./ansible_collections/ska_collections
 
 CLUSTERAPI_APPLY ?= false ## Apply workload cluster: true or false - default: false
 CLUSTERAPI_AC_BRANCH ?= bang-105-add-byoh
@@ -19,30 +22,32 @@ vars:
 	@echo "\033[36mclusterapi:\033[0m"
 	@echo "INVENTORY=$(INVENTORY)"
 	@echo "PLAYBOOKS_HOSTS=$(PLAYBOOKS_HOSTS)"
+	@echo "ANSIBLE_PLAYBOOK_ARGUMENTS=$(ANSIBLE_PLAYBOOK_ARGUMENTS)"
+	@echo "ANSIBLE_EXTRA_VARS=$(ANSIBLE_EXTRA_VARS)"
 
 clusterapi-install-base:  ## Install base for management server
-	ansible-playbook ./ansible_collections/ska_collections/docker_base/playbooks/containers.yml \
-	-i $(INVENTORY) \
+	ansible-playbook $(PLAYBOOKS_DIR)/docker_base/playbooks/containers.yml \
+	-i $(INVENTORY) $(ANSIBLE_PLAYBOOK_ARGUMENTS) $(ANSIBLE_EXTRA_VARS) \
 	--extra-vars "target_hosts=$(PLAYBOOKS_HOSTS)" \
 	--limit "management-cluster"
 
 # clusterapi-management:
 clusterapi-management: clusterapi-install-base  ## Install Minikube management cluster
-	ansible-playbook ./ansible_collections/ska_collections/minikube/playbooks/minikube.yml \
-	-i $(INVENTORY) \
+	ansible-playbook $(PLAYBOOKS_DIR)/minikube/playbooks/minikube.yml \
+	-i $(INVENTORY) $(ANSIBLE_PLAYBOOK_ARGUMENTS) $(ANSIBLE_EXTRA_VARS) \
 	--extra-vars "target_hosts=$(PLAYBOOKS_HOSTS)" \
 	--limit "management-cluster" --tags "build"
 
 # clusterapi:
 clusterapi: clusterapi-check-hosts clusterapi-management clusterapi-velero-backups  ## Install clusterapi component
-	ansible-playbook ./ansible_collections/ska_collections/clusterapi/playbooks/clusterapi.yml \
-	-i $(INVENTORY) \
+	ansible-playbook $(PLAYBOOKS_DIR)/clusterapi/playbooks/clusterapi.yml \
+	-i $(INVENTORY) $(ANSIBLE_PLAYBOOK_ARGUMENTS) $(ANSIBLE_EXTRA_VARS) \
 	--extra-vars "target_hosts=$(PLAYBOOKS_HOSTS)" \
 	--limit "management-cluster"
 
 clusterapi-createworkload: clusterapi-check-cluster-type  ## Template workload manifest and deploy
-	ansible-playbook ./ansible_collections/ska_collections/clusterapi/playbooks/createworkload.yml \
-	-i $(INVENTORY) \
+	ansible-playbook $(PLAYBOOKS_DIR)/clusterapi/playbooks/createworkload.yml \
+	-i $(INVENTORY) $(ANSIBLE_PLAYBOOK_ARGUMENTS) $(ANSIBLE_EXTRA_VARS) \
 	--extra-vars "target_hosts=$(PLAYBOOKS_HOSTS)" \
 	--extra-vars "capi_kustomize_overlay=$(CLUSTERAPI_CLUSTER_TYPE)" \
 	--extra-vars '{"cluster_apply": $(CLUSTERAPI_APPLY)}' \
@@ -53,38 +58,38 @@ clusterapi-createworkload: clusterapi-check-cluster-type  ## Template workload m
 clusterapi-byoh-reset:  ## Reset workload hosts
 	ANSIBLE_CONFIG="$(PLAYBOOKS_ROOT_DIR)/ansible.cfg" \
 	ANSIBLE_SSH_ARGS="-o ControlPersist=30m -o StrictHostKeyChecking=no -F $(PLAYBOOKS_ROOT_DIR)/ssh.config" \
-	ansible-playbook ./ansible_collections/ska_collections/clusterapi/playbooks/reset-byoh.yml \
-	-i $(INVENTORY) \
+	ansible-playbook $(PLAYBOOKS_DIR)/clusterapi/playbooks/reset-byoh.yml \
+	-i $(INVENTORY) $(ANSIBLE_PLAYBOOK_ARGUMENTS) $(ANSIBLE_EXTRA_VARS) \
 	--extra-vars "target_hosts=$(PLAYBOOKS_HOSTS)" -v
 
 clusterapi-byoh:  ## Deploy byoh agent and tokens to workload hosts
 	ANSIBLE_CONFIG="$(PLAYBOOKS_ROOT_DIR)/ansible.cfg" \
 	ANSIBLE_SSH_ARGS="-o ControlPersist=30m -o StrictHostKeyChecking=no -F $(PLAYBOOKS_ROOT_DIR)/ssh.config" \
-	ansible-playbook ./ansible_collections/ska_collections/docker_base/playbooks/containerd.yml \
-	-i $(INVENTORY) \
+	ansible-playbook $(PLAYBOOKS_DIR)/docker_base/playbooks/containerd.yml \
+	-i $(INVENTORY) $(ANSIBLE_PLAYBOOK_ARGUMENTS) $(ANSIBLE_EXTRA_VARS) \
 	--extra-vars "target_hosts=$(PLAYBOOKS_HOSTS)" -v
 
 	ANSIBLE_CONFIG="$(PLAYBOOKS_ROOT_DIR)/ansible.cfg" \
 	ANSIBLE_SSH_ARGS="-o ControlPersist=30m -o StrictHostKeyChecking=no -F $(PLAYBOOKS_ROOT_DIR)/ssh.config" \
-	ansible-playbook ./ansible_collections/ska_collections/clusterapi/playbooks/byoh.yml \
-	-i $(INVENTORY) \
+	ansible-playbook $(PLAYBOOKS_DIR)/clusterapi/playbooks/byoh.yml \
+	-i $(INVENTORY) $(ANSIBLE_PLAYBOOK_ARGUMENTS) $(ANSIBLE_EXTRA_VARS) \
 	--extra-vars "target_hosts=$(PLAYBOOKS_HOSTS)" -v
 
 clusterapi-destroy-management:  ## Destroy Minikube management cluster
-	ansible-playbook ./ansible_collections/ska_collections/minikube/playbooks/minikube.yml \
-	-i $(INVENTORY) \
+	ansible-playbook $(PLAYBOOKS_DIR)/minikube/playbooks/minikube.yml \
+	-i $(INVENTORY) $(ANSIBLE_PLAYBOOK_ARGUMENTS) $(ANSIBLE_EXTRA_VARS) \
 	--extra-vars "target_hosts=$(PLAYBOOKS_HOSTS)" \
 	--limit "management-cluster" --tags "destroy"
 
 clusterapi-imagebuilder:  ## Build and upload OS Image Builder Kubernetes image
-	ansible-playbook ./ansible_collections/ska_collections/clusterapi/playbooks/imagebuilder.yml \
-	-i $(INVENTORY) \
+	ansible-playbook $(PLAYBOOKS_DIR)/clusterapi/playbooks/imagebuilder.yml \
+	-i $(INVENTORY) $(ANSIBLE_PLAYBOOK_ARGUMENTS) $(ANSIBLE_EXTRA_VARS) \
 	--extra-vars "target_hosts=$(PLAYBOOKS_HOSTS)" \
 	--limit "management-cluster" -v
 
 clusterapi-velero-backups:  ## Configure Velero backups on Minikube management cluster
-	ansible-playbook ./ansible_collections/ska_collections/minikube/playbooks/velero_backups.yml \
-	-i $(INVENTORY) \
+	ansible-playbook $(PLAYBOOKS_DIR)/minikube/playbooks/velero_backups.yml \
+	-i $(INVENTORY) $(ANSIBLE_PLAYBOOK_ARGUMENTS) $(ANSIBLE_EXTRA_VARS) \
 	--extra-vars "target_hosts=$(PLAYBOOKS_HOSTS)" \
 	--extra-vars "cloud_config=$(CLUSTERAPI_CLOUD_CONFIG)" \
 	--extra-vars "cloud=$(CLUSTERAPI_CLOUD)" \
