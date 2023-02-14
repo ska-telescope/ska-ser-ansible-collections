166de2f872f320fc49160428ca0b5ed26ec99ab4
Pedro Osório <pedroosorio.eeic@gmail.com>
2023-02-14 10:48:19 +0000

    ST-1481: Refactored Makefile, haproxy and conan search roles

--- resources/jobs/nexus.mk
+++ resources/jobs/nexus.mk
@@ -1,23 +1,10 @@
+.PHONY: vars help install apply-patch
 .DEFAULT_GOAL := help
 ANSIBLE_PLAYBOOK_ARGUMENTS ?=
 INVENTORY ?= $(PLAYBOOKS_ROOT_DIR)
 PLAYBOOKS_DIR ?= ./ansible_collections/ska_collections/nexus/playbooks
 TESTS_DIR ?= ./ansible_collections/ska_collections/nexus/tests
-
-NEXUS_DEFAULT_ADMIN_PASSWORD ?=
-NEXUS_VAULT_ADMIN_PASSWORD ?= 'whatwhat'
-NEXUS_VAULT_USER_PASSWORD_GITLAB ?= 'whatwhat'
-NEXUS_VAULT_USER_PASSWORD_PUBLISHER ?= 'whatwhat'
-NEXUS_VAULT_EMAIL_SERVER_PASSWORD ?= 'whatwhat'
-NEXUS_VAULT_USER_PASSWORD_QUARANTINER ?= 'whatwhat'
-NEXUS_VAULT_LDAP_CONN_PASSWD ?= 'whatwhat'
-NEXUS_WEBHOOK_SECRET_KEY ?= 'whatwhat'
-NEXUS_APT_BIONIC_INTERNAL_KEY ?= 'whatwhat'
-NEXUS_APT_BIONIC_INTERNAL_KEY_PASSPHRASE ?= 'whatwhat'
-NEXUS_APT_BIONIC_QUARENTINE_KEY ?= 'whatwhat'
-NEXUS_APT_BIONIC_QUARENTINE_KEY_PASSPHRASE ?= 'whatwhat'
-
-.PHONY: vars help install apply-patch update-and-patch make-patch
+NEXUS_OSS_INSTALL_DIR ?= ./ansible_collections/ansible-thoteam.nexus3-oss
 
 # define overides for above variables in here
 -include $(BASE_PATH)/PrivateRules.mak
@@ -27,68 +14,41 @@ ifndef PLAYBOOKS_HOSTS
 	$(error PLAYBOOKS_HOSTS is undefined)
 endif
 
-vars:  ## List Variables
-	@echo "Current variable settings:"
+vars:
+	@echo "\033[36mNexus:\033[0m"
 	@echo "INVENTORY=$(INVENTORY)"
-	@echo "NEXUS_DEFAULT_ADMIN_PASSWORD=$(NEXUS_DEFAULT_ADMIN_PASSWORD)"
-	@echo "NEXUS_VAULT_ADMIN_PASSWORD=$(NEXUS_VAULT_ADMIN_PASSWORD)"
-	@echo "NEXUS_VAULT_USER_PASSWORD_GITLAB=$(NEXUS_VAULT_USER_PASSWORD_GITLAB)"
-	@echo "NEXUS_VAULT_USER_PASSWORD_PUBLISHER=$(NEXUS_VAULT_USER_PASSWORD_PUBLISHER)"
-	@echo "NEXUS_VAULT_EMAIL_SERVER_PASSWORD=$(NEXUS_VAULT_EMAIL_SERVER_PASSWORD)"
-	@echo "NEXUS_VAULT_USER_PASSWORD_QUARANTINER=$(NEXUS_VAULT_USER_PASSWORD_QUARANTINER)"
-	@echo "NEXUS_VAULT_LDAP_CONN_PASSWD=$(NEXUS_VAULT_LDAP_CONN_PASSWD)"
-	@echo "NEXUS_WEBHOOK_SECRET_KEY=$(NEXUS_WEBHOOK_SECRET_KEY)"
-	@echo "NEXUS_APT_BIONIC_INTERNAL_KEY=$(NEXUS_APT_BIONIC_INTERNAL_KEY)"
-	@echo "NEXUS_APT_BIONIC_INTERNAL_KEY_PASSPHRASE=$(NEXUS_APT_BIONIC_INTERNAL_KEY_PASSPHRASE)"
-	@echo "NEXUS_APT_BIONIC_QUARENTINE_KEY=$(NEXUS_APT_BIONIC_QUARENTINE_KEY)"
-	@echo "NEXUS_APT_BIONIC_QUARENTINE_KEY_PASSPHRASE=$(NEXUS_APT_BIONIC_QUARENTINE_KEY_PASSPHRASE)"
+	@echo "PLAYBOOKS_HOSTS=$(PLAYBOOKS_HOSTS)"
 
 install: check_hosts apply-patch # apply-patch  ## Deploy Nexus
-	ANSIBLE_FILTER_PLUGINS=./ansible_collections/ansible-thoteam.nexus3-oss/filter_plugins \
+	ANSIBLE_FILTER_PLUGINS=$(NEXUS_OSS_INSTALL_DIR)/filter_plugins \
 	ansible-playbook $(PLAYBOOKS_DIR)/deploy.yml \
-	-i $(INVENTORY) \
-	$(ANSIBLE_PLAYBOOK_ARGUMENTS) \
-	--extra-vars " \
-		nexus_default_admin_password=$(NEXUS_DEFAULT_ADMIN_PASSWORD) \
-		nexus_vault_admin_password=$(NEXUS_VAULT_ADMIN_PASSWORD) \
-		nexus_vault_user_password_gitlab=$(NEXUS_VAULT_USER_PASSWORD_GITLAB) \
-		nexus_vault_user_password_publisher=$(NEXUS_VAULT_USER_PASSWORD_PUBLISHER) \
-		nexus_vault_email_server_password=$(NEXUS_VAULT_EMAIL_SERVER_PASSWORD) \
-		nexus_vault_user_password_quarantiner=$(NEXUS_VAULT_USER_PASSWORD_QUARANTINER) \
-		nexus_vault_ldap_conn_passwd=$(NEXUS_VAULT_LDAP_CONN_PASSWD) \
-		nexus_webhook_secret_key=$(NEXUS_WEBHOOK_SECRET_KEY) \
-		nexus_apt_bionic_internal_key=$(NEXUS_APT_BIONIC_INTERNAL_KEY) \
-		nexus_apt_bionic_internal_key_passphrase=$(NEXUS_APT_BIONIC_INTERNAL_KEY_PASSPHRASE) \
-		nexus_apt_bionic_quarentine_key=$(NEXUS_APT_BIONIC_QUARENTINE_KEY) \
-		nexus_apt_bionic_quarentine_key_passphrase=$(NEXUS_APT_BIONIC_QUARENTINE_KEY_PASSPHRASE) \
-		target_hosts=$(PLAYBOOKS_HOSTS) \
-	"
+	-i $(INVENTORY) $(ANSIBLE_PLAYBOOK_ARGUMENTS) $(ANSIBLE_EXTRA_VARS) \
+	--extra-vars "target_hosts=$(PLAYBOOKS_HOSTS)"
 
 apply-patch:  ## apply patch to upstream nexus3-oss
-	# patch the nexus install for the oss edition
-	if [ -f ./ansible_collections/ansible-thoteam.nexus3-oss/.patched ]; then \
-	echo "Already patched !"; \
+	@if [ ! -d "$(NEXUS_OSS_INSTALL_DIR)" ]; then \
+		echo "You need to install dependent packages first. Please run 'make playbooks ac-install-dependencies'"; \
+		exit 1; \
+	fi
+	@if [ -f $(NEXUS_OSS_INSTALL_DIR)/.patched ]; then \
+		echo "Nexus OSS collection already patched"; \
 	else \
-	git apply --directory ./ansible_collections/ansible-thoteam.nexus3-oss \
-		./ansible_collections/ska_collections/nexus/resources/nexus3-oss.patch --verbose --unsafe-paths && \
-	touch ./ansible_collections/ansible-thoteam.nexus3-oss/.patched; \
+		git apply --directory $(NEXUS_OSS_INSTALL_DIR) \
+			./ansible_collections/ska_collections/nexus/resources/nexus3-oss.patch --verbose --unsafe-paths && \
+		touch $(NEXUS_OSS_INSTALL_DIR)/.patched; \
+		echo "Nexus OSS collection patched"; \
 	fi
 
 test-apt-proxy-cache:  ## Test apt proxy and caching behavior
 	ansible-playbook $(TESTS_DIR)/apt-proxy-cache.yml \
 	-i $(INVENTORY) $(ANSIBLE_PLAYBOOK_ARGUMENTS) $(ANSIBLE_EXTRA_VARS) \
-	--extra-vars " \
-		nexus_vault_admin_password=$(NEXUS_VAULT_ADMIN_PASSWORD) \
-		target_hosts=$(PLAYBOOKS_HOSTS) \
-	"
+	--extra-vars "target_hosts=$(PLAYBOOKS_HOSTS)"
 
 test-oci-proxy-cache:  ## Test oci proxy and caching behavior
 	ansible-playbook $(TESTS_DIR)/oci-proxy-cache.yml \
 	-i $(INVENTORY) $(ANSIBLE_PLAYBOOK_ARGUMENTS) $(ANSIBLE_EXTRA_VARS) \
-	--extra-vars " \
-		nexus_vault_admin_password=$(NEXUS_VAULT_ADMIN_PASSWORD) \
-		target_hosts=$(PLAYBOOKS_HOSTS) \
-	"
+	--extra-vars "target_hosts=$(PLAYBOOKS_HOSTS)"
+
 help: ## Show Help
 	@echo "Nexus targets - make playbooks nexus <target>:"
 	@grep -E '^[0-9a-zA-Z_-]+:.*?## .*$$' $(MAKEFILE_LIST) | sort | awk 'BEGIN {FS = ": .*?## "}; {printf "\033[36m%-30s\033[0m %s\n", $$1, $$2}'
