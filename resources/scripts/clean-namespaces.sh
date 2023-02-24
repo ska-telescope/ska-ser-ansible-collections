378bc97274e5c7339d045503315842334ff7bff7
J. Bruno Morgado <jb.morgado@gmail.com>
2023-02-24 09:49:01 +0000

    ST-1465: rewrite namespace cleaner

---
+++ resources/scripts/clean-namespaces.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+DECODED_INPUT=$(echo $1 | base64 -d | xargs)
+for RULE in $DECODED_INPUT; do
+        AGE=$(echo $RULE | sed 's#.*:\([0-9]\+\)$#\1#')
+        REGEX=$(echo $RULE | sed 's#:[0-9]\+$##')
+        echo "Terminating all namespaces matching '$REGEX' older than '$AGE' seconds."
+        NAMESPACES=$(kubectl get namespaces --field-selector status.phase=Active -o json | jq --arg regex "$REGEX" --arg age $AGE -r '.items[] | select ((now - (.metadata.creationTimestamp | fromdateiso8601)) > ($age|tonumber)) | select(.metadata.name | test($regex)).metadata.name')
+        for NS in $NAMESPACES; do
+                kubectl delete namespace $NS
+        done
+done
