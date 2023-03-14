bbcaa53f175124fe298ad2a2dba893f3d4ffeee3
mpaulo_csw <mariana.paulo@criticalsoftware.com>
2023-03-14 18:55:57 +0000

    ST-1526: Adding playbook to manage new gitlab users

---
+++ resources/jobs/gitlab-management.mk
@@ -0,0 +1,26 @@
+.PHONY: help
+.DEFAULT_GOAL := help
+ANSIBLE_PLAYBOOK_ARGUMENTS ?=
+ANSIBLE_EXTRA_VARS ?=
+INVENTORY ?= $(PLAYBOOKS_ROOT_DIR)
+PLAYBOOKS_DIR ?= ./ansible_collections/ska_collections/gitlab_management/playbooks
+
+-include $(BASE_PATH)/PrivateRules.mak
+
+add_gitlab_user: ## Add user to SKAO default groups
+	ansible-playbook $(PLAYBOOKS_DIR)/add_user.yml \
+	$(ANSIBLE_PLAYBOOK_ARGUMENTS) $(ANSIBLE_EXTRA_VARS) \
+	--extra-vars "target_hosts=$(PLAYBOOKS_HOSTS)" \
+	--extra-vars "gitlab_user_to_manage=$(GITLAB_USER_TO_MANAGE)" \
+	--extra-vars "gitlab_access_token=$(GITLAB_ACCESS_TOKEN)"
+
+remove_gitlab_user: ## Add user to SKAO default groups
+	ansible-playbook $(PLAYBOOKS_DIR)/remove_user.yml \
+	$(ANSIBLE_PLAYBOOK_ARGUMENTS) $(ANSIBLE_EXTRA_VARS) \
+	--extra-vars "target_hosts=$(PLAYBOOKS_HOSTS)" \
+	--extra-vars "gitlab_user_to_manage=$(GITLAB_USER_TO_MANAGE)" \
+	--extra-vars "gitlab_access_token=$(GITLAB_ACCESS_TOKEN)"
+
+help: ## Show Help
+	@echo "Gitlab_runner targets - make playbooks gitlab_management <target>:"
+	@grep -E '^[0-9a-zA-Z_-]+:.*?## .*$$' $(MAKEFILE_LIST) | sort | awk 'BEGIN {FS = ": .*?## "}; {printf "\033[36m%-30s\033[0m %s\n", $$1, $$2}'
