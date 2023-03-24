d7fcec355e30288a316fed5be9c951270b49b7b1
Bruno_Ribeiro <bruno.engelec@gmail.com>
2023-03-24 13:43:28 +0000

    BANG-111: template instead of copy for cronjob

--- ansible_collections/ska_collections/gateway/roles/cron/files/monitor-skagitrunners-stfc.sh
+++ ansible_collections/ska_collections/gateway/roles/cron/files/monitor-skagitrunners-stfc.sh
@@ -2,7 +2,7 @@
 notification_url=https://heartbeat.uptimerobot.com/m787767935-6c18b1c3356566b927f3484dc8bc662b536dde5d
 interval="1h"
 
-query=`curl -k -g "https://${cron_prometheus_ip}:9090/api/v1/query?query=sum(increase(gitlab_runner_errors_total{level=~'error|fatal|panic'}[${interval}]))"`
+query=`curl -k -g "https://{{ cron_prometheus_ip }}:9090/api/v1/query?query=sum(increase(gitlab_runner_errors_total{level=~'error|fatal|panic'}[${interval}]))"`
 gitrst=`echo "${query}" | jq -r .data.result[0].value[1]`
 
 echo "Start: " $(date)
