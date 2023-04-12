acab60a999adca8d6144e0f753d96b49a8dc297a
J. Bruno Morgado <jb.morgado@gmail.com>
2023-04-12 13:16:20 +0100

    ST-1608: Tidy up

--- resources/jobs/k8s.mk
+++ resources/jobs/k8s.mk
@@ -137,7 +137,7 @@ ifneq (,$(findstring nvidia,$(TAGS)))
 	-i $(INVENTORY) $(ANSIBLE_PLAYBOOK_ARGUMENTS) $(ANSIBLE_EXTRA_VARS) \
 	--extra-vars "target_hosts=$(PLAYBOOKS_HOSTS)" \
 	--extra-vars "k8s_kubeconfig=$(K8S_KUBECONFIG)" \
-	--tags "$(TAGS)" \
+	--tags "$(TAGS)"
 endif
 
 k8s-velero-backups:  ## Configure Velero backups on Kubernetes
