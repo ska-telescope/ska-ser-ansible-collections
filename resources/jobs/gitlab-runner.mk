83c0fdf08d2dca0a0e614d5962883f64748d85fd
Mariana Paulo <mariana.paulo@criticalsoftware.com>
2023-04-19 14:45:01 +0100

    ST-1584: Attempt at adding registration token

--- resources/jobs/gitlab-runner.mk
+++ resources/jobs/gitlab-runner.mk
@@ -63,6 +63,7 @@ k8s_runner_helm: tidy  ## Deploy runners
 	-i $(INVENTORY) $(ANSIBLE_PLAYBOOK_ARGUMENTS) $(ANSIBLE_EXTRA_VARS) \
 	--extra-vars "target_hosts=$(PLAYBOOKS_HOSTS)" \
 	--extra-vars "k8s_kubeconfig=$(K8S_KUBECONFIG)" \
+	--extra-vars "gitlab_runner_registration_token=$(GITLAB_RUNNER_REGISTRATION_TOKEN)" \
 	$(GITLAB_RUNNER_TAG_LIST_ARG)
 
 deploy_minio: tidy  ## Deploy Minio
