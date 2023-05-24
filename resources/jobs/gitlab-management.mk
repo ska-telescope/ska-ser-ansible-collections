c5868f6f56f2500ee6443c16cfb1323b997cded9
Mariana Paulo <mariana.paulo@criticalsoftware.com>
2023-05-24 18:41:26 +0100

    ST-1650: Fixing minor issues left from last ip sprint

--- resources/jobs/gitlab-management.mk
+++ resources/jobs/gitlab-management.mk
@@ -4,6 +4,7 @@ ANSIBLE_PLAYBOOK_ARGUMENTS ?=
 ANSIBLE_EXTRA_VARS ?=
 INVENTORY ?= $(PLAYBOOKS_ROOT_DIR)
 PLAYBOOKS_DIR ?= ./ansible_collections/ska_collections/gitlab_management/playbooks
+PLAYBOOKS_HOSTS ?= localhost
 
 -include $(BASE_PATH)/PrivateRules.mak
 
@@ -18,17 +19,15 @@ endif
 add_gitlab_user: check_input_args ## Add user to SKAO default groups
 	ansible-playbook $(PLAYBOOKS_DIR)/add_user.yml \
 	$(ANSIBLE_PLAYBOOK_ARGUMENTS) $(ANSIBLE_EXTRA_VARS) \
-	--extra-vars "target_hosts=$(PLAYBOOKS_HOSTS)" \
 	--extra-vars "gitlab_user_to_manage=$(GITLAB_USER_TO_MANAGE)" \
 	--extra-vars "gitlab_access_token=$(GITLAB_ACCESS_TOKEN)"
 
 remove_gitlab_user: check_input_args ## Add user to SKAO default groups
 	ansible-playbook $(PLAYBOOKS_DIR)/remove_user.yml \
 	$(ANSIBLE_PLAYBOOK_ARGUMENTS) $(ANSIBLE_EXTRA_VARS) \
-	--extra-vars "target_hosts=$(PLAYBOOKS_HOSTS)" \
 	--extra-vars "gitlab_user_to_manage=$(GITLAB_USER_TO_MANAGE)" \
 	--extra-vars "gitlab_access_token=$(GITLAB_ACCESS_TOKEN)"
 
 help: ## Show Help
-	@echo "Gitlab_runner targets - make playbooks gitlab_management <target>:"
+	@echo "Gitlab_management targets - make playbooks gitlab_management <target>:"
 	@grep -E '^[0-9a-zA-Z_-]+:.*?## .*$$' $(MAKEFILE_LIST) | sort | awk 'BEGIN {FS = ": .*?## "}; {printf "\033[36m%-30s\033[0m %s\n", $$1, $$2}'
