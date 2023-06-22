ed0ff44ce0c28e44079f4f632f2e30f1a6df59da
Pete Prior <pete.prior@skao.int>
2023-06-22 10:54:41 +0100

    ST-1671 Updated logic

--- resources/scripts/clean-namespaces.sh
+++ resources/scripts/clean-namespaces.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 DECODED_INPUT=$(echo $1 | base64 -d | xargs)
+SHARED_NAMESPACE_REGEX="^dev-shared-"
 
 for RULE in $DECODED_INPUT; do
         AGE=$(echo $RULE | sed 's#.*:\([0-9]\+\)$#\1#')
@@ -13,7 +14,7 @@ for RULE in $DECODED_INPUT; do
 done
 
 echo "Terminating any shared namespaces that have an expired TTL"
-NAMESPACES=$(kubectl get namespaces --field-selector status.phase=Active -o json | jq -r '.items[] | select ((.metadata.annotations.SKA_SHARED_ENV_TTL | fromdateiso8601) < now)')
+NAMESPACES=$(kubectl get namespaces --field-selector status.phase=Active -o json | jq --arg regex "$SHARED_NAMESPACE_REGEX" -r '.items[] | select ((.metadata.annotations.SKA_SHARED_ENV_TTL | fromdateiso8601) < now) |  select(.metadata.name | test($regex)).metadata.name')
 for NS in $NAMESPACES; do
 	kubectl delete namespace $NS
 done
